edd09d3a57f7cd422f06d206cea670776bb730d3
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-09-09 10:45:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, NavLink, Link } from "react-router-dom";
+import { Routes, Route, NavLink} from "react-router-dom";
 // import PropTypes from 'prop-types';
 import Home from '../pages/Home';
 import Movies from '../pages/Movies';
