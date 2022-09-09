079217441c8080574c0e0317f622ef009f7d9c1d
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-09-09 10:34:13 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import { Routes, Route, NavLink, Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
+import NotFound from '../pages/NotFound';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      goit-react-hw-05-movies
+      <nav className='nav'>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Movies = props => {
+  return (
+    <div>
+      Movies
+    </div>
+  )
+}
+
+Movies.propTypes = {
+
+}
+
+export default Movies
